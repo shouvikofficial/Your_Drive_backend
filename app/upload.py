4751105b5a2268a75a6301dd1d8f0864a3e09209
shouvikofficial <shouvikdhaliofficial@gmail.com>
2026-02-11 18:39:46 +0600

    redis fix

--- app/upload.py
+++ app/upload.py
@@ -22,7 +22,6 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 r = redis.Redis.from_url(
     REDIS_URL,
     decode_responses=True,
-    ssl=True,
 )
 
 # ============================================================
